cccfeeb8b36c9d2e700bec25a406043cb093c5b0
Alexandru Rusu <62150899+alexandrusu1@users.noreply.github.com>
2025-12-12 21:12:46 +0200

    Co-authored-by: adelinprelipcean <aamoviescontact@gmail.com>

--- inamic.gd
+++ inamic.gd
@@ -1,20 +1,16 @@
 extends CharacterBody3D
 
-# --- SETĂRI ---
 @export var viteza: float = 2.5
 @export var player_target: Node3D 
-@export var distanta_de_atac: float = 1.5 # Distanța la care dă Game Over
+@export var distanta_de_atac: float = 1.5
 
-# Luăm gravitația din setările jocului
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _ready():
-	# 1. FIX PENTRU ROTATIE (CA SĂ NU FUGĂ CU SPATELE)
 	var model_intern = get_node_or_null("Running")
 	if model_intern:
 		model_intern.rotation_degrees.y = 180
 	
-	# 2. PORNIRE ANIMAȚIE
 	var anim_player = find_child("AnimationPlayer", true, false)
 	if anim_player:
 		var lista_animatii = anim_player.get_animation_list()
@@ -23,12 +19,10 @@ func _ready():
 			anim_player.play(lista_animatii[0])
 
 func _physics_process(delta):
-	# Aplicăm gravitația
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
 	if player_target:
-		# --- MIȘCAREA ---
 		var target_pos = player_target.global_position
 		target_pos.y = global_position.y 
 		
@@ -38,11 +32,8 @@ func _physics_process(delta):
 		velocity.x = directie.x * viteza
 		velocity.z = directie.z * viteza
 		
-		# --- VERIFICARE GAME OVER (NOU) ---
-		# Calculăm distanța reală 3D dintre inamic și jucător
 		var distanta = global_position.distance_to(player_target.global_position)
 		
-		# Dacă e mai aproape de 1.5 metri, ai pierdut
 		if distanta < distanta_de_atac:
 			game_over()
 	else:
@@ -51,14 +42,6 @@ func _physics_process(delta):
 
 	move_and_slide()
 
-# --- FUNCȚIA DE FINAL ---
 func game_over():
 	print("TE-A PRINS! Game Over.")
-	
-	# Aici poți alege ce se întâmplă:
-	
-	# Varianta 1: Resetează nivelul imediat
 	get_tree().reload_current_scene()
-	
-	# Varianta 2: Iese din joc (scoate comentariul de mai jos dacă vrei asta)
-	# get_tree().quit()
